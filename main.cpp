224158f340f440b53a5e35517a53a092fea95720
Khurram Amin <khurram@Khurrams-MacBook-Pro.local>
2016-11-13 15:16:07 +0300

    minor bug fixes

--- main.cpp
+++ main.cpp
@@ -19,19 +19,19 @@ int main()
 	while(1)
 	{
 		dataT = 0;
-		readTempRawData(dataT);
+		mpu9250.readTempRawData(dataT);
 
 		for (int i=0; i<3; i++){ dataA[i] = 0; }
-		readAccelroRawData(dataA);
+		mpu9250.readAccelroRawData(dataA);
 
 		for (int i=0; i<3; i++){ dataG[i] = 0; }
-		readGyroRawData(dataG);
+		mpu9250.readGyroRawData(dataG);
 
 		for (int i=0; i<3; i++){ dataM[i] = 0; }
-		readMagnetoRawData(dataM);
+		mpu9250.readMagnetoRawData(dataM);
 
 
-		sprintf("Temp = %d\nAccelX = %d\nAccelY = %d\nAccelZ = %d\nGyroX = %d\nGyroY = %d\nGyroZ = %d\nMagntX = %d\nMagntY = %d\nMagntZ = %d\n\n\n", dataT, dataA[0]*mpu9250.getAccelroResolution, dataA[1]*mpu9250.getAccelroResolution, dataA[2]*mpu9250.getAccelroResolution, dataG[0]*mpu9250.getGyroResolution, dataG[1]*mpu9250.getGyroResolution, dataG[2]*mpu9250.getGyroResolution, dataM[0]*mpu9250.getMagnetoResolution, dataM[1]*mpu9250.getMagnetoResolution, dataM[2]*mpu9250.getMagnetoResolution);
+		sprintf("Temp = %d\nAccelX = %d\nAccelY = %d\nAccelZ = %d\nGyroX = %d\nGyroY = %d\nGyroZ = %d\nMagntX = %d\nMagntY = %d\nMagntZ = %d\n\n\n", *dataT, *dataA[0]*mpu9250.getAccelroResolution, *dataA[1]*mpu9250.getAccelroResolution, *dataA[2]*mpu9250.getAccelroResolution, *dataG[0]*mpu9250.getGyroResolution, *dataG[1]*mpu9250.getGyroResolution, *dataG[2]*mpu9250.getGyroResolution, *dataM[0]*mpu9250.getMagnetoResolution, *dataM[1]*mpu9250.getMagnetoResolution, *dataM[2]*mpu9250.getMagnetoResolution);
 
 		delay(10);
 	}
