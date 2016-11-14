853c4d89200cdae3266905391b5f6bf9d855117d
Khurram Amin <khurram@Khurrams-MacBook-Pro.local>
2016-11-14 22:06:43 +0300

    minor bug fixes

--- main.cpp
+++ main.cpp
@@ -22,35 +22,36 @@ int main()
 	mpu9250.initAK8963(dataMB);
 	delay(10);
 	
+	while(1){
+		if( digitalRead(0) )
+		{
+			dataT = 0;
+			mpu9250.readTempRawData(&dataT);
 
-	while( digitalRead(0) )
-	{
-		dataT = 0;
-		mpu9250.readTempRawData(&dataT);
+			for (int i=0; i<3; i++){ dataA[i] = 0; }
+			mpu9250.readAccelroRawData(dataA);
 
-		for (int i=0; i<3; i++){ dataA[i] = 0; }
-		mpu9250.readAccelroRawData(dataA);
+			for (int i=0; i<3; i++){ dataG[i] = 0; }
+			mpu9250.readGyroRawData(dataG);
 
-		for (int i=0; i<3; i++){ dataG[i] = 0; }
-		mpu9250.readGyroRawData(dataG);
-
-		for (int i=0; i<3; i++){ dataM[i] = 0; }
-		mpu9250.readMagnetoRawData(dataM);
+			for (int i=0; i<3; i++){ dataM[i] = 0; }
+			mpu9250.readMagnetoRawData(dataM);
 
 
-		//sprintf("Temp = %d\nAccelX = %d\nAccelY = %d\nAccelZ = %d\nGyroX = %d\nGyroY = %d\nGyroZ = %d\nMagntX = %d\nMagntY = %d\nMagntZ = %d\n\n\n", 1.0f*dataT, 1.0f*dataA[0]*mpu9250.getAccelroResolution, 1.0f*dataA[1]*mpu9250.getAccelroResolution, 1.0f*dataA[2]*mpu9250.getAccelroResolution, 1.0f*dataG[0]*mpu9250.getGyroResolution, 1.0f*dataG[1]*mpu9250.getGyroResolution, 1.0f*dataG[2]*mpu9250.getGyroResolution, 1.0f*dataM[0]*mpu9250.getMagnetoResolution, 1.0f*dataM[1]*mpu9250.getMagnetoResolution, 1.0f*dataM[2]*mpu9250.getMagnetoResolution);
-		cout<< "Temp = " << 1.0f*dataT << endl;
-		cout<< "AccelX = " << 1.0f*dataA[0]*mpu9250.getAccelroResolution() << endl;
-		cout<< "AccelY = " << 1.0f*dataA[1]*mpu9250.getAccelroResolution() << endl;
-		cout<< "AccelZ = " << 1.0f*dataA[2]*mpu9250.getAccelroResolution() << endl;
-		cout<< "GyroX = " << 1.0f*dataG[0]*mpu9250.getGyroResolution() << endl;
-		cout<< "GyroY = " << 1.0f*dataG[1]*mpu9250.getGyroResolution() << endl;
-		cout<< "GyroZ = " << 1.0f*dataG[2]*mpu9250.getGyroResolution() << endl;
-		cout<< "MagntX = " << 1.0f*dataM[0]*mpu9250.getMagnetoResolution() << endl;
-		cout<< "MagntY = " << 1.0f*dataM[1]*mpu9250.getMagnetoResolution() << endl;
-		cout<< "MagntZ = " << 1.0f*dataM[2]*mpu9250.getMagnetoResolution() << endl;
-		cout<<endl<<endl<<endl;
-		
-		mpu9250.readByte(MPU9250_ADDRESS, INT_STATUS);
+			//sprintf("Temp = %d\nAccelX = %d\nAccelY = %d\nAccelZ = %d\nGyroX = %d\nGyroY = %d\nGyroZ = %d\nMagntX = %d\nMagntY = %d\nMagntZ = %d\n\n\n", 1.0f*dataT, 1.0f*dataA[0]*mpu9250.getAccelroResolution, 1.0f*dataA[1]*mpu9250.getAccelroResolution, 1.0f*dataA[2]*mpu9250.getAccelroResolution, 1.0f*dataG[0]*mpu9250.getGyroResolution, 1.0f*dataG[1]*mpu9250.getGyroResolution, 1.0f*dataG[2]*mpu9250.getGyroResolution, 1.0f*dataM[0]*mpu9250.getMagnetoResolution, 1.0f*dataM[1]*mpu9250.getMagnetoResolution, 1.0f*dataM[2]*mpu9250.getMagnetoResolution);
+			cout<< "Temp = " << 1.0f*dataT << endl;
+			cout<< "AccelX = " << 1.0f*dataA[0]*mpu9250.getAccelroResolution() << endl;
+			cout<< "AccelY = " << 1.0f*dataA[1]*mpu9250.getAccelroResolution() << endl;
+			cout<< "AccelZ = " << 1.0f*dataA[2]*mpu9250.getAccelroResolution() << endl;
+			cout<< "GyroX = " << 1.0f*dataG[0]*mpu9250.getGyroResolution() << endl;
+			cout<< "GyroY = " << 1.0f*dataG[1]*mpu9250.getGyroResolution() << endl;
+			cout<< "GyroZ = " << 1.0f*dataG[2]*mpu9250.getGyroResolution() << endl;
+			cout<< "MagntX = " << 1.0f*dataM[0]*mpu9250.getMagnetoResolution() << endl;
+			cout<< "MagntY = " << 1.0f*dataM[1]*mpu9250.getMagnetoResolution() << endl;
+			cout<< "MagntZ = " << 1.0f*dataM[2]*mpu9250.getMagnetoResolution() << endl;
+			cout<<endl<<endl<<endl;
+			
+			mpu9250.readByte(MPU9250_ADDRESS, INT_STATUS);
+		}
 	}
 }
