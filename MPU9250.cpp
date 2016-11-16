a150c0db44315bdbc7a0e87ee407fbe1c9383bbe
Khurram Amin <khurram@Khurrams-MacBook-Pro.local>
2016-11-17 02:12:01 +0300

    minor bug fixes

--- MPU9250.cpp
+++ MPU9250.cpp
@@ -473,9 +473,9 @@ void MPU9250::calibrateAK8963Mag(float * dest1, float * dest2)
 {
 	uint16_t ii = 0, sample_count = 0;
 	int32_t mag_bias[3] = {0, 0, 0}, mag_scale[3] = {0, 0, 0};
-	int16_t mag_max[3] = new int16_t[3];
+	int16_t mag_max = new int16_t[3];
 	mag_max[0] = -32767; mag_max[1] = -32767; mag_max[2] = -32767;
-	int16_t mag_min[3]  = new int16_t[3];
+	int16_t mag_min  = new int16_t[3];
 	mag_min[0] = 32768; mag_min[1] = 32768; mag_min[2] = 32768;
 	int16_t mag_temp[3] = {0, 0, 0};
 
