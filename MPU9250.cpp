b6afdf1e04b4187343da60f6df2eae92ed11ca39
Khurram Amin <khurram@Khurrams-MacBook-Pro.local>
2016-11-17 04:23:51 +0300

    minor bug fixes

--- MPU9250.cpp
+++ MPU9250.cpp
@@ -479,7 +479,7 @@ void MPU9250::calibrateMPU9250(float * gyroBias, float * accelBias)
 void MPU9250::calibrateAK8963Mag(float * dest1, float * dest2) 
 {
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	this.whoAmIAK8963();
+	this->whoAmIAK8963();
 	uint16_t ii = 0, sample_count = 0;
 	int32_t mag_bias[3] = {0, 0, 0}, mag_scale[3] = {0, 0, 0};
 
