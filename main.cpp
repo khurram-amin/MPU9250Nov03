dccfbd0f9ce48a32bc576f604b6efbeeef9155ec
Khurram Amin <khurram@Khurrams-MacBook-Pro.local>
2016-11-17 04:38:39 +0300

    minor bug fixes

--- main.cpp
+++ main.cpp
@@ -80,12 +80,12 @@ int main()
 	mag_bias[1]  = (mag_max[1] + mag_min[1])/2;  // get average y mag bias in counts
 	mag_bias[2]  = (mag_max[2] + mag_min[2])/2;  // get average z mag bias in counts
 
-	float * dest1 = new float[3];
+	float * magBias = new float[3];
 	float * dest2 = new float[3];
 
-	dest1[0] = (float) mag_bias[0]*mpu9250.magnetoResolution*mpu9250.magCalibration[0];  // save mag biases in G for main program
-	dest1[1] = (float) mag_bias[1]*mpu9250.magnetoResolution*mpu9250.magCalibration[1];   
-	dest1[2] = (float) mag_bias[2]*mpu9250.magnetoResolution*mpu9250.magCalibration[2];  
+	magBias[0] = (float) mag_bias[0]*mpu9250.magnetoResolution*mpu9250.magCalibration[0];  // save mag biases in G for main program
+	magBias[1] = (float) mag_bias[1]*mpu9250.magnetoResolution*mpu9250.magCalibration[1];   
+	magBias[2] = (float) mag_bias[2]*mpu9250.magnetoResolution*mpu9250.magCalibration[2];  
 
 	// Get soft iron correction estimate
 	mag_scale[0]  = (mag_max[0] - mag_min[0])/2;  // get average x axis max chord length in counts
@@ -95,9 +95,9 @@ int main()
 	float avg_rad = mag_scale[0] + mag_scale[1] + mag_scale[2];
 	avg_rad /= 3.0;
 
-	dest2[0] = avg_rad/((float)mag_scale[0]);
-	dest2[1] = avg_rad/((float)mag_scale[1]);
-	dest2[2] = avg_rad/((float)mag_scale[2]);
+	magScale[0] = avg_rad/((float)mag_scale[0]);
+	magScale[1] = avg_rad/((float)mag_scale[1]);
+	magScale[2] = avg_rad/((float)mag_scale[2]);
 
 	cout << (int) mag_max[0] << endl;
 	cout << (int) mag_min[0] << endl;
